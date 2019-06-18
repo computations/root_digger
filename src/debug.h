7ad164b24ed4f7ffa37aacd6c4cc8da583baecfb
computations <bwbettisworth@gmail.com>
2019-06-18 16:12:43 +0200

    fixs an edge case with debug printouts

--- src/debug.h
+++ src/debug.h
@@ -18,27 +18,29 @@ extern bool __VERBOSE__;
 #define EMIT_DEBUG_FLAG __VERBOSE__
 
 #define print_clock                                                            \
-  {                                                                            \
+  do {                                                                         \
     if (DEBUG_IF_FLAG && EMIT_DEBUG_FLAG) {                                    \
       fprintf(stderr, "[%f] ",                                                 \
               ((double)clock() - CLOCK_START) / CLOCKS_PER_SEC);               \
     }                                                                          \
-  };
+  } while (0)
 
 #define debug_print(fmt, ...)                                                  \
-  {                                                                            \
+  do {                                                                         \
     if (DEBUG_IF_FLAG && EMIT_DEBUG_FLAG) {                                    \
       print_clock;                                                             \
       fprintf(stderr, "[%s:%s:%d]: " fmt "\n", __FILE__, __func__, __LINE__,   \
               __VA_ARGS__);                                                    \
     }                                                                          \
-  }
+  } while (0)
 
 #define debug_string(x)                                                        \
-  { debug_print("%s", x) }
+  do {                                                                         \
+    debug_print("%s", x);                                                      \
+  } while (0)
 
 #define print_trace()                                                          \
-  {                                                                            \
+  do {                                                                         \
     if (DEBUG_IF_FLAG) {                                                       \
       void *callstack[128];                                                    \
       int frames = backtrace(callstack, 128);                                  \
@@ -51,41 +53,50 @@ extern bool __VERBOSE__;
         fprintf(stderr, "%s\n", bt_symbols[i]);                                \
       }                                                                        \
     }                                                                          \
-  }
+  } while (0)
 
 #define assert_string(cond, comment)                                           \
-  if (DEBUG_IF_FLAG && EMIT_DEBUG_FLAG) {                                      \
-    {                                                                          \
-      if (!(cond)) {                                                           \
-        print_clock;                                                           \
-        fprintf(stderr,                                                        \
-                "assertion \"%s\" failed: file: %s, line: %d, comment: %s\n",  \
-                #cond, __FILE__, __LINE__, comment);                           \
-        abort();                                                               \
+  do {                                                                         \
+    if (DEBUG_IF_FLAG && EMIT_DEBUG_FLAG) {                                    \
+      {                                                                        \
+        if (!(cond)) {                                                         \
+          print_clock;                                                         \
+          fprintf(                                                             \
+              stderr,                                                          \
+              "assertion \"%s\" failed: file: %s, line: %d, comment: %s\n",    \
+              #cond, __FILE__, __LINE__, comment);                             \
+          abort();                                                             \
+        }                                                                      \
       }                                                                        \
     }                                                                          \
-  }
+  } while (0)
 
 #define turn_on_progress()                                                     \
-  { __PROGRESS_BAR_FLAG__ = true; }
+  do {                                                                         \
+    __PROGRESS_BAR_FLAG__ = true;                                              \
+  } while (0)
 
 #define turn_off_progress()                                                    \
-  { __PROGRESS_BAR_FLAG__ = false; }
+  do {                                                                         \
+    __PROGRESS_BAR_FLAG__ = false;                                             \
+  } while (0)
 
 #define toggle_progress()                                                      \
-  { __PROGRESS_BAR_FLAG__ = !__PROGRESS_BAR_FLAG__; }
+  do {                                                                         \
+    __PROGRESS_BAR_FLAG__ = !__PROGRESS_BAR_FLAG__;                            \
+  } while (0)
 
 #define print_progress(done, total)                                            \
-  {                                                                            \
+  do {                                                                         \
     if (__PROGRESS_BAR_FLAG__) {                                               \
       struct winsize w;                                                        \
       ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);                                    \
       print_progress_cols(done, total, w.ws_col);                              \
     }                                                                          \
-  }
+  } while (0)
 
 #define print_progress_cols(done, total, cols)                                 \
-  {                                                                            \
+  do {                                                                         \
     if (__PROGRESS_BAR_FLAG__) {                                               \
       size_t adj_cols = cols - 5;                                              \
       if (done == 0)                                                           \
@@ -127,14 +138,14 @@ extern bool __VERBOSE__;
       fprintf(stdout, "\e[0m");                                                \
       fflush(stdout);                                                          \
     }                                                                          \
-  }
+  } while (0)
 
 #define finish_progress()                                                      \
-  {                                                                            \
+  do {                                                                         \
     if (__PROGRESS_BAR_FLAG__) {                                               \
       fprintf(stdout, "\n");                                                   \
       fflush(stdout);                                                          \
     }                                                                          \
-  }
+  } while (0)
 
 #endif
