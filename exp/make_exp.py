4ced0922929215a0e46acbad463dda4c0ae2a616
computations <bwbettisworth@gmail.com>
2019-06-19 11:43:41 +0200

    adds some janky checkpointing to the experiments

--- exp/make_exp.py
+++ exp/make_exp.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import subprocess
 import sys
+import datetime
 
 if not shutil.which("indelible"):
     print("Please add indelible to your path")
@@ -27,6 +28,35 @@ RD = os.path.abspath("../bin/rd") + " --msa {msa} --tree {tree} --model {model} 
 model_file = "random_unsym.model"
 freqs_file = 'uniform.freqs'
 
+def check_done(path):
+    return os.path.exists(os.path.join(path, ".done"))
+
+def check_done_indel(path):
+    if check_done(path):
+        with open(os.path.join(path, ".done")) as done_file:
+            for line in done_file:
+                if line.find('indel') != -1:
+                    return True
+    return False
+
+def check_done_rd(path):
+    if check_done(path):
+        with open(os.path.join(path, ".done")) as done_file:
+            for line in done_file:
+                if line.find('rd') != -1:
+                    return True
+    return False
+
+
+def set_indel_done(path):
+    with open(os.path.join(path, ".done"), 'a') as done_file:
+        done_file.write("indel:" + datetime.datetime.now().isoformat())
+
+def set_rd_done(path):
+    with open(os.path.join(path, ".done"), 'a') as done_file:
+        done_file.write("rd:" + datetime.datetime.now().isoformat())
+
+
 if not os.path.exists('active_exp'):
     os.mkdir('active_exp')
 
@@ -45,14 +75,18 @@ for taxa in [10, 100, 1000, 10000]:
             cf.write(CONTROL_FILE.format(tree=tree, sites=sites))
         old_dir = os.getcwd()
         os.chdir(exp_dir)
-        subprocess.run("indelible", shell=True, check=True)
-        print(RD.format(msa="seqs_TRUE.phy", tree=os.path.join("../..",
-            tree_file), model=os.path.join('../../',model_file),
-            freqs=os.path.join("../../", freqs_file)).split(' '))
-        rd_output = subprocess.run(RD.format(msa="seqs_TRUE.phy", tree=os.path.join("../..",
-            tree_file), model=os.path.join('../../',model_file),
-            freqs=os.path.join("../../", freqs_file)).split(' '),
-            capture_output=True)
-        with open('rd_output', 'w') as rd_outfile:
-            rd_outfile.write(rd_output.stdout.decode('utf-8'))
+        if not check_done_indel('.'):
+            subprocess.run("indelible", shell=True, check=True)
+            set_indel_done('.')
+        if not check_done_rd('.'):
+            print(RD.format(msa="seqs_TRUE.phy", tree=os.path.join("../..",
+                tree_file), model=os.path.join('../../',model_file),
+                freqs=os.path.join("../../", freqs_file)).split(' '))
+            rd_output = subprocess.run(RD.format(msa="seqs_TRUE.phy", tree=os.path.join("../..",
+                tree_file), model=os.path.join('../../',model_file),
+                freqs=os.path.join("../../", freqs_file)).split(' '),
+                capture_output=True)
+            with open('rd_output', 'w') as rd_outfile:
+                rd_outfile.write(rd_output.stdout.decode('utf-8'))
+            set_rd_done('.')
         os.chdir(old_dir)
