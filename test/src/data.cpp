88f2077106f3702b9b9865be01d4a57f21146926
computations <bwbettisworth@gmail.com>
2019-08-05 13:43:53 +0200

    sped up tests

--- test/src/data.cpp
+++ test/src/data.cpp
@@ -6,16 +6,19 @@
 std::vector<std::string> data_keys = {
     "single",
     "101.phy",
+    "10.fasta",
 };
 
-std::map<std::string, std::pair<std::string, std::string>>
-    data_files_dna = {
-        {"single",
-         {__stringify_base(DATA_DIRECTORY_DNA_ABS) + "single.phy",
-          __stringify_base(DATA_DIRECTORY_TREE_ABS) + "single.tree"}},
-        {"101.phy",
-         {__stringify_base(DATA_DIRECTORY_DNA_ABS) + "101.phy",
-          __stringify_base(DATA_DIRECTORY_TREE_ABS) + "101.tree"}},
+std::map<std::string, std::pair<std::string, std::string>> data_files_dna = {
+    {"single",
+     {__stringify_base(DATA_DIRECTORY_DNA_ABS) + "single.phy",
+      __stringify_base(DATA_DIRECTORY_TREE_ABS) + "single.tree"}},
+    {"101.phy",
+     {__stringify_base(DATA_DIRECTORY_DNA_ABS) + "101.phy",
+      __stringify_base(DATA_DIRECTORY_TREE_ABS) + "101.tree"}},
+    {"10.fasta",
+     {__stringify_base(DATA_DIRECTORY_DNA_ABS) + "10.fasta",
+      __stringify_base(DATA_DIRECTORY_TREE_ABS) + "10.tree"}},
 };
 
 std::map<std::string, std::string> check_trees = {
