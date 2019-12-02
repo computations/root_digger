92fde5e4491219e700122844369c7885de2ec032
computations <bwbettisworth@gmail.com>
2019-12-02 15:25:34 +0100

    Refactors and adds options for the cli

--- src/main.cpp
+++ src/main.cpp
@@ -28,11 +28,6 @@ int __VERBOSE__ = EMIT_LEVEL_WARNING;
 #define BUILD_DATE_STRING STRINGIFY(BUILD_DATE)
 
 void print_version() {
-  /*
-  std::cout << "Version: " << GIT_REV_STRING << "\n"
-            << "Build Commit: " << GIT_COMMIT_STRING << "\n"
-            << "Build Date: " << BUILD_DATE_STRING << std::endl;
-  */
   debug_print(EMIT_LEVEL_IMPORTANT, "Version: %s", GIT_REV_STRING);
   debug_print(EMIT_LEVEL_IMPORTANT, "Build Commit: %s", GIT_COMMIT_STRING);
   debug_print(EMIT_LEVEL_IMPORTANT, "Build Date: %s", BUILD_DATE_STRING);
@@ -44,17 +39,41 @@ void print_run_header(
   time_t st = std::chrono::system_clock::to_time_t(start_time);
   char time_string[256];
   std::strftime(time_string, sizeof(time_string), "%F %T", std::localtime(&st));
-  // std::cout << "Runing Root Digger \n";
   debug_string(EMIT_LEVEL_IMPORTANT, "Running Root Digger");
   print_version();
-  /*
-  std::cout << "Started: " << std::put_time(std::localtime(&st), "%F %T")
-            << "\n"
-            << "Seed: " << seed << std::endl;
-            */
   debug_print(EMIT_LEVEL_IMPORTANT, "Started: %s", time_string);
   debug_print(EMIT_LEVEL_IMPORTANT, "Seed: %lu", seed);
 }
+
+class initialized_flag_t {
+public:
+  enum value_t {
+    uninitalized,
+    initialized_true,
+    initialized_false,
+  };
+  initialized_flag_t() : value(value_t::uninitalized){};
+  constexpr initialized_flag_t(const value_t &v) : value(v) {}
+  constexpr bool operator==(const initialized_flag_t &rhs) {
+    return rhs.value == value;
+  }
+  constexpr bool operator!=(const initialized_flag_t &rhs) {
+    return rhs.value != value;
+  }
+  constexpr bool initalized() const { return value != value_t::uninitalized; }
+  initialized_flag_t &operator=(const initialized_flag_t &rhs) {
+    value = rhs.value;
+    return *this;
+  }
+  bool convert_with_default(bool default_value) {
+    if (value == value_t::uninitalized)
+      return default_value;
+    return value == value_t::initialized_true;
+  }
+
+private:
+  value_t value;
+};
 
 void print_usage() {
   std::cout
@@ -109,6 +128,27 @@ size_t data_type_to_states(std::string data_type) {
       "Unrecognized data type, unable to determine correct number of states");
 }
 
+struct cli_options_t {
+  std::string msa_filename;
+  std::string tree_filename;
+  std::string output_tree_filename;
+  std::string model_filename;
+  std::string freqs_filename;
+  std::string partition_filename;
+  std::string data_type;
+  uint64_t seed = std::random_device()();
+  size_t min_roots = 1;
+  double root_ratio = 0.05;
+  double abs_tolerance = 1e-7;
+  double factor = 1e4;
+  double br_tolerance = 1e-12;
+  double bfgs_tol = 1e-7;
+  unsigned int states = 0;
+  bool silent = false;
+  bool exhaustive = false;
+  initialized_flag_t early_stop;
+};
+
 int main(int argv, char **argc) {
   auto start_time = std::chrono::system_clock::now();
   static struct option long_opts[] = {
@@ -129,10 +169,11 @@ int main(int argv, char **argc) {
       {"partition", required_argument, 0, 0},  /* 14 */
       {"treefile", required_argument, 0, 0},   /* 15 */
       {"exhaustive", no_argument, 0, 0},       /* 16 */
-      {"no-early-stop", no_argument, 0, 0},    /* 17 */
-      {"data-type", required_argument, 0, 0},  /* 18 */
-      {"version", no_argument, 0, 0},          /* 19 */
-      {"debug", no_argument, 0, 0},            /* 20 */
+      {"early-stop", no_argument, 0, 0},       /* 17 */
+      {"no-early-stop", no_argument, 0, 0},    /* 18 */
+      {"data-type", required_argument, 0, 0},  /* 19 */
+      {"version", no_argument, 0, 0},          /* 20 */
+      {"debug", no_argument, 0, 0},            /* 21 */
       {0, 0, 0, 0},
   };
 
@@ -143,90 +184,79 @@ int main(int argv, char **argc) {
   try {
     int c;
     int index = 0;
-    std::string msa_filename;
-    std::string tree_filename;
-    std::string output_tree_filename;
-    std::string model_filename;
-    std::string freqs_filename;
-    std::string partition_filename;
-    std::string data_type;
-    uint64_t seed = std::random_device()();
-    size_t min_roots = 1;
-    double root_ratio = 0.05;
-    double abs_tolerance = 1e-7;
-    double factor = 1e4;
-    double br_tolerance = 1e-12;
-    double bfgs_tol = 1e-7;
-    unsigned int states = 0;
-    bool silent = false;
-    bool exhaustive = false;
-    bool early_stop = true;
+    cli_options_t cli_options;
     while ((c = getopt_long_only(argv, argc, "", long_opts, &index)) == 0) {
       debug_print(EMIT_LEVEL_DEBUG, "parsing option index: %d", index);
       switch (index) {
       case 0: // msa
-        msa_filename = optarg;
+        cli_options.msa_filename = optarg;
         break;
       case 1: // tree
-        tree_filename = optarg;
+        cli_options.tree_filename = optarg;
         break;
       case 2: // model
-        model_filename = optarg;
+        cli_options.model_filename = optarg;
         break;
       case 3: // freqs
-        freqs_filename = optarg;
+        cli_options.freqs_filename = optarg;
         break;
       case 4: // seed
-        seed = atol(optarg);
+        cli_options.seed = atol(optarg);
         break;
       case 5: // states
-        states = atol(optarg);
+        cli_options.states = atol(optarg);
         break;
       case 6: // verbose
         __VERBOSE__ += 1;
         break;
       case 7: // silent
         __VERBOSE__ = 0;
-        silent = true;
+        cli_options.silent = true;
         break;
       case 8: // min-roots
-        min_roots = atol(optarg);
+        cli_options.min_roots = atol(optarg);
         break;
       case 9: // root-ratio
-        root_ratio = atof(optarg);
+        cli_options.root_ratio = atof(optarg);
         break;
       case 10: // atol
-        abs_tolerance = atof(optarg);
+        cli_options.abs_tolerance = atof(optarg);
         break;
       case 11: // brtol
-        br_tolerance = atof(optarg);
+        cli_options.br_tolerance = atof(optarg);
         break;
       case 12: // bfgs_tol
-        bfgs_tol = atof(optarg);
+        cli_options.bfgs_tol = atof(optarg);
         break;
       case 13: // factor
-        factor = atof(optarg);
+        cli_options.factor = atof(optarg);
         break;
       case 14: // partition
-        partition_filename = optarg;
+        cli_options.partition_filename = optarg;
         break;
       case 15: // treefile
-        output_tree_filename = optarg;
+        cli_options.output_tree_filename = optarg;
         break;
       case 16: // exhaustive
-        exhaustive = true;
+        cli_options.exhaustive = true;
+        if (!cli_options.early_stop.initalized()) {
+          cli_options.early_stop = initialized_flag_t::initialized_false;
+        }
         break;
-      case 17: // exhaustive
-        early_stop = false;
+      case 17: // early-stop
+        cli_options.early_stop = initialized_flag_t::initialized_true;
         break;
-      case 18: // data-type
-        data_type = optarg;
-        states = data_type_to_states(data_type);
+      case 18: // no-early-stop
+        cli_options.early_stop = initialized_flag_t::initialized_false;
         break;
-      case 19: // version
+      case 19: // data-type
+        cli_options.data_type = optarg;
+        cli_options.states = data_type_to_states(cli_options.data_type);
+        break;
+      case 20: // version
         print_version();
         return 0;
-      case 20: // debug
+      case 21: // debug
         __VERBOSE__ = EMIT_LEVEL_DEBUG;
         break;
       default:
@@ -234,22 +264,22 @@ int main(int argv, char **argc) {
       }
     }
 
-    if (msa_filename.empty()) {
+    if (cli_options.msa_filename.empty()) {
       print_usage();
       return 1;
     }
-    if (tree_filename.empty()) {
+    if (cli_options.tree_filename.empty()) {
       print_usage();
       return 1;
     }
 
     model_params_t model_params;
 
-    if (!model_filename.empty()) {
-      model_params = parse_model_file(model_filename);
-      states =
+    if (!cli_options.model_filename.empty()) {
+      model_params = parse_model_file(cli_options.model_filename);
+      cli_options.states =
           static_cast<unsigned int>(std::ceil(std::sqrt(model_params.size())));
-    } else if (states == 0) {
+    } else if (cli_options.states == 0) {
       std::cout << "Please give either a model file, or a number of states to "
                    "the model"
                 << std::endl;
@@ -257,16 +287,19 @@ int main(int argv, char **argc) {
       return 1;
     }
 
-    if (!silent)
-      print_run_header(start_time, seed);
+    if (!cli_options.silent)
+      print_run_header(start_time, cli_options.seed);
 
-    debug_print(EMIT_LEVEL_INFO, "abs_tolerance: %f", abs_tolerance);
+    debug_print(EMIT_LEVEL_INFO, "abs_tolerance: %f",
+                cli_options.abs_tolerance);
+
     const pll_state_t *map = nullptr;
-
-    if (states == 4)
+    if (cli_options.states == 4)
       map = pll_map_nt;
-    if (states == 20)
+    else if (cli_options.states == 20)
       map = pll_map_aa;
+    else
+      throw std::runtime_error("Data Type is not supported");
 
     if (map == nullptr) {
       throw std::invalid_argument(
@@ -274,47 +307,60 @@ int main(int argv, char **argc) {
     }
 
     std::vector<msa_t> msa;
-    if (partition_filename.empty()) {
-      msa.emplace_back(msa_filename, map, states);
+    if (cli_options.partition_filename.empty()) {
+      msa.emplace_back(cli_options.msa_filename, map, cli_options.states);
     } else {
-      msa_t unparted_msa{msa_filename, map, states};
-      msa = unparted_msa.partition(parse_partition_file(partition_filename));
+      msa_t unparted_msa{cli_options.msa_filename, map, cli_options.states};
+      msa = unparted_msa.partition(
+          parse_partition_file(cli_options.partition_filename));
     }
 
-    rooted_tree_t tree{tree_filename};
+    rooted_tree_t tree{cli_options.tree_filename};
 
-    model_t model{tree, msa, seed, early_stop};
+    debug_print(
+        EMIT_LEVEL_INFO, "early stop: %d",
+        cli_options.early_stop.convert_with_default(!cli_options.exhaustive));
+    model_t model{
+        tree, msa, cli_options.seed,
+        cli_options.early_stop.convert_with_default(!cli_options.exhaustive)};
     try {
       model.initialize_partitions(msa);
     } catch (const invalid_empirical_frequencies_exception &) {
       model.initialize_partitions_uniform_freqs(msa);
     }
+
     root_location_t final_rl;
     double final_lh;
     std::string final_tree_string;
-    if (!exhaustive) {
-      auto tmp = model.optimize_all(min_roots, root_ratio, abs_tolerance,
-                                    bfgs_tol, br_tolerance, factor);
+    if (!cli_options.exhaustive) {
+      auto tmp =
+          model.optimize_all(cli_options.min_roots, cli_options.root_ratio,
+                             cli_options.abs_tolerance, cli_options.bfgs_tol,
+                             cli_options.br_tolerance, cli_options.factor);
       final_rl = tmp.first;
       final_lh = tmp.second;
       final_tree_string = std::move(model.rooted_tree(final_rl).newick());
     } else {
-      auto tmp = model.exhaustive_search(abs_tolerance, bfgs_tol, br_tolerance,
-                                         factor);
+      auto tmp = model.exhaustive_search(
+          cli_options.abs_tolerance, cli_options.bfgs_tol,
+          cli_options.br_tolerance, cli_options.factor);
       final_rl = tmp.first;
       final_lh = tmp.second;
       final_tree_string = std::move(model.unrooted_tree().newick());
     }
-    if (!silent) {
+    if (!cli_options.silent) {
       debug_print(EMIT_LEVEL_IMPORTANT, "Final LogLH: %.5f", final_lh);
     }
     std::cout << final_tree_string << std::endl;
     auto end_time = std::chrono::system_clock::now();
+
     std::chrono::duration<double> duration = end_time - start_time;
-    if (!silent)
+
+    if (!cli_options.silent)
       std::cout << "Inference took: " << duration.count() << "s" << std::endl;
-    if (!output_tree_filename.empty()) {
-      std::ofstream outfile{output_tree_filename};
+
+    if (!cli_options.output_tree_filename.empty()) {
+      std::ofstream outfile{cli_options.output_tree_filename};
       outfile << final_tree_string;
     }
 
