09e8f08b8033cd4df0ca7972bde1e855aa15ecbe
computations <bwbettisworth@gmail.com>
2019-12-02 16:25:13 +0100

    Adds a progress level to the debug print macro

--- src/debug.h
+++ src/debug.h
@@ -24,12 +24,13 @@ extern int __VERBOSE__;
 #define EMIT_LEVEL_IMPORTANT 0
 #define EMIT_LEVEL_ERROR 1
 #define EMIT_LEVEL_WARNING 2
-#define EMIT_LEVEL_INFO 3
-#define EMIT_LEVEL_DEBUG 4
+#define EMIT_LEVEL_PROGRESS 3
+#define EMIT_LEVEL_INFO 4
+#define EMIT_LEVEL_DEBUG 5
 
 #define print_clock                                                            \
   do {                                                                         \
-    fprintf(stdout, "[%.2f] ",                                                   \
+    fprintf(stdout, "[%.2f] ",                                                 \
             ((double)clock() - CLOCK_START) / CLOCKS_PER_SEC);                 \
   } while (0)
 
