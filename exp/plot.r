1c986c4b58db52fab878355a9f221ad5c007b658
computations <bwbettisworth@gmail.com>
2020-01-02 14:36:13 +0100

    refactors histogram plotting

--- exp/plot.r
+++ exp/plot.r
@@ -2,23 +2,28 @@
 
 library("data.table")
 library("ggplot2")
-json_data <- fread("./simulated_exp/test_results_times.csv")
+timeing_data <- fread("./simulated_exp/test_results_times.csv")
 
-trees <- unique(json_data$tree)
+trees <- unique(timeing_data$tree)
 taxa <- rep("Taxa", length(trees))
 
-aligns <- unique(json_data$alignment)
+aligns <- unique(timeing_data$alignment)
 site <- rep("Sites", length(aligns))
 
-json_data[, count:=.N, by=list(tree, alignment)]
+timeing_data[, count:=.N, by=list(tree, alignment)]
 colMax <- function(data) sapply(data, max, na.rm= TRUE)
-data_len = colMax(json_data)['count']
-json_data[, tree_name := paste(tree, "Taxa"), by=list(tree)]
-json_data[, alignment_name := paste(alignment, "Sites"), by=list(alignment)]
+data_len = colMax(timeing_data)['count']
+timeing_data[, tree_name := paste(tree, "Taxa"), by=list(tree)]
+timeing_data[, alignment_name := paste(alignment, "Sites"), by=list(alignment)]
 
 bins = ceiling(2 * data_len^(1/3))
 
-ggplot(data = json_data, aes(x = rd_time)) + geom_histogram(aes(y = ..count../sum(..count..)), bins = bins) + 
+ggplot(data = timeing_data, aes(x = rd_time)) + geom_histogram(aes(y = ..count../sum(..count..)), bins = bins) + 
     facet_wrap(tree_name ~ alignment_name, scales="free") + labs(x = "Time(s)", y = "Density") + theme_minimal()
 
-ggsave("ggtest.png")
+ggsave("./simulated_exp/rd_time_hist.png")
+
+ggplot(data = timeing_data, aes(x = iq_time)) + geom_histogram(aes(y = ..count../sum(..count..)), bins = bins) + 
+    facet_wrap(tree_name ~ alignment_name, scales="free") + labs(x = "Time(s)", y = "Density") + theme_minimal()
+
+ggsave("./simulated_exp/iq_time_hist.png")
